b50a2be28b6462eb379731b0fb32fe69a8705408
Yash <yashpatel256@gmail.com>
2016-12-05 20:04:46 -0500

    showed details about specific templates using routeparams

--- app/templates/template-detail.controller.js
+++ app/templates/template-detail.controller.js
@@ -5,7 +5,21 @@
     'use strict';
 
     angular.module('templateStore.templates')
-        .controller('TemplateDetailCtrl', ['$scope', function ($scope) {
+        .controller('TemplateDetailCtrl', ['$scope','templateService','$routeParams','$filter', function ($scope, templateService, $routeParams, $filter) {
+            var templateId = $routeParams.id;
+            templateService.getData().then(
+                function (data) {
+                    $scope.templates = $filter('filter')(data, function (d) {
+                        return d.id == templateId;
+                    })[0];
+                    $scope.mainImage = $scope.templates.images[0].name;
+                }, function (err) {
+                    console.log(err);
+                }
+            );
 
+            $scope.setImage = function (image) {
+                $scope.mainImage = image.name;
+            }
         }]);
 })();
